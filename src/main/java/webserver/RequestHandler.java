81de90abf524634ae3a3d93f35bcc65c705896db
hae young joo <jhy753@gmail.com>
2017-07-23 10:40:39 +0900

    http 웹 서버 구현. 요구사항1 - index.html 응답하기 http://localhost:8080/index.html로 접속했을 때 webapp 디렉토리의 index.html 파일을 읽어 클라이언트에 응답한다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,15 +1,22 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import util.HttpRequestUtils;
+
 public class RequestHandler extends Thread {
+	
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
@@ -17,23 +24,56 @@ public class RequestHandler extends Thread {
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
-
+    
     public void run() {
+    
+    	
+    	//클라이언
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
 
+        //클라이언트와 서버의 통신을 inputstream 과 outputstream 을 이용한다. 
+        // try 안에 스트림을 얻어오면 catch 에서는 자동으로 close가 된다. 
+        
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+               
+        	// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	
+        	//line 단위로 데이터를 얻어온다. 
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in,"UTF-8"));
+        	
+        	String line = br.readLine();
+        	
+        	if(line == null ) {
+        		return ;
+        	}
+        	
+        	//첫번 째 라인에서 index.html을 추출한다. 
+        	String url = HttpRequestUtils.getUrl(line);
+        	
+        	while(!"".equals(line)) {
+        		
+        		log.debug("header :{}",line );
+        		line = br.readLine();
+        		
+        	}
+       
+        	DataOutputStream dos = new DataOutputStream(out);
+            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
+            
             response200Header(dos, body.length);
+            
             responseBody(dos, body);
+            
         } catch (IOException e) {
-            log.error(e.getMessage());
+            
+        	log.error(e.getMessage());
+        
         }
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    	
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
@@ -43,9 +83,10 @@ public class RequestHandler extends Thread {
             log.error(e.getMessage());
         }
     }
-
+    
     private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
+    
+    	try {
             dos.write(body, 0, body.length);
             dos.flush();
         } catch (IOException e) {
--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -7,11 +7,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WebServer {
+	
     private static final Logger log = LoggerFactory.getLogger(WebServer.class);
+    
     private static final int DEFAULT_PORT = 8080;
 
     public static void main(String args[]) throws Exception {
+    	
         int port = 0;
+        
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
         } else {
@@ -19,13 +23,16 @@ public class WebServer {
         }
 
         // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
-
         try (ServerSocket listenSocket = new ServerSocket(port)) {
+        	
             log.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
+            // 사용자의 요청이 있을 때 까지 대기한다.
+            // 사용자의 요청이 많을수 있다.  while문으로 계속 요청을 잡는다. 
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
+            	
+            	//요청이 들어오면  클라이언트의 연결을 담당하는 소켓을 requestHandler에게 전달하면서 새로운 스레드를 실행하는 방식으로 멀티 스레드 프로그래밍을 지원하고 있다. 
                 RequestHandler requestHandler = new RequestHandler(connection);
                 requestHandler.start();
             }
