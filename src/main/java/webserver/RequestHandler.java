6c567a89a0f64551113caf0f3249816a0f60b54d
hae young joo <jhy753@gmail.com>
2017-07-24 13:08:51 +0900

    http 웹서버 구축 요구사항2 - GET 방식으로 회원가입하기.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,10 +9,12 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import model.User;
 import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
@@ -48,16 +50,34 @@ public class RequestHandler extends Thread {
         		return ;
         	}
         	
-        	//첫번 째 라인에서 index.html을 추출한다. 
+       
+         	//첫번 째 라인에서 index.html을 추출한다. 
         	String url = HttpRequestUtils.getUrl(line);
+        	if( url.startsWith("/user") ) {
+        		
+        		int index = url.indexOf("?");
+        		String requestPath = url.substring(0,index);
+        		String queryString = url.substring(index+1);
+        		Map<String,String> params =HttpRequestUtils.parseQueryString(queryString);
+        		
+        		User user = new User(params.get("userId"), params.get("password"), params.get("name"), params.get("email"));
+        		
+        		log.debug("User : {}", user);
+        		
+        		url = "/index.html";
+        		
+        	}
+        
         	
+        	/*
         	while(!"".equals(line)) {
         		
         		log.debug("header :{}",line );
         		line = br.readLine();
         		
         	}
-       
+        	*/
+        	
         	DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
             
